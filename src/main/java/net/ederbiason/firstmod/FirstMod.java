ac45db4d58c91e081c473e860a8c7bb7a6609442
ederbiason <ederbiason@hotmail.com>
2025-02-20 15:09:56 -0300

    feat: add first items

--- src/main/java/net/ederbiason/firstmod/FirstMod.java
+++ src/main/java/net/ederbiason/firstmod/FirstMod.java
@@ -1,6 +1,8 @@
 package net.ederbiason.firstmod;
 
 import com.mojang.logging.LogUtils;
+import net.ederbiason.firstmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -29,7 +31,7 @@ public class FirstMod {
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
-
+        ModItems.register(modEventBus);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
@@ -43,7 +45,14 @@ public class FirstMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.ALEXANDRITE);
+            event.accept(ModItems.RAW_ALEXANDRITE);
+        }
 
+        if(event.getTabKey() == CreativeModeTabs.FOOD_AND_DRINKS) {
+            event.accept(ModItems.PROFITEROLE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
