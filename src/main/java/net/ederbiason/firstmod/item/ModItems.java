b69f5d1c7cce037360f4b50f9d61b7a64b6d1ff8
ederbiason <ederbiason@hotmail.com>
2025-02-26 16:02:11 -0300

    feat: add custom advanced item (chisel)

--- src/main/java/net/ederbiason/firstmod/item/ModItems.java
+++ src/main/java/net/ederbiason/firstmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.ederbiason.firstmod.item;
 
 import net.ederbiason.firstmod.FirstMod;
+import net.ederbiason.firstmod.item.custom.ChiselItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -15,6 +16,8 @@ public class ModItems {
     public static final RegistryObject<Item> RAW_ALEXANDRITE = ITEMS.register("raw_alexandrite", () -> new Item(new Item.Properties()));
 
     public static final RegistryObject<Item> PROFITEROLE = ITEMS.register("profiterole", () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> CHISEL = ITEMS.register("chisel", () -> new ChiselItem(new Item.Properties().durability(32)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
