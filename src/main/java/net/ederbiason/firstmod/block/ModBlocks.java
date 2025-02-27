c51576df36f85f9d173a595a01510d9b5f84d22a
ederbiason <ederbiason@hotmail.com>
2025-02-27 16:01:57 -0300

    feat: add custom advanced block

--- src/main/java/net/ederbiason/firstmod/block/ModBlocks.java
+++ src/main/java/net/ederbiason/firstmod/block/ModBlocks.java
@@ -1,6 +1,7 @@
 package net.ederbiason.firstmod.block;
 
 import net.ederbiason.firstmod.FirstMod;
+import net.ederbiason.firstmod.block.custom.MagicBlock;
 import net.ederbiason.firstmod.item.ModItems;
 import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
@@ -35,6 +36,9 @@ public class ModBlocks {
     public static final RegistryObject<Block> ALEXANDRITE_DEEPSLATE_ORE = registerBlock("alexandrite_deepslate_ore",
             () -> new DropExperienceBlock(UniformInt.of(3 ,6), BlockBehaviour.Properties.of()
                     .strength(5f).requiresCorrectToolForDrops().sound(SoundType.DEEPSLATE)));
+
+    public static final RegistryObject<Block> MAGIC_BLOCK = registerBlock("magic_block",
+            () -> new MagicBlock(BlockBehaviour.Properties.of().strength(2f).requiresCorrectToolForDrops()));
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
